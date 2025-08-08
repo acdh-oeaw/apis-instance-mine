71065ddc16ef5babbf9d7d1d5d03c10208c80efc
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-08-08 17:48:29 +0200

    fix: wrong path

--- apis_ontology/models.py
+++ apis_ontology/models.py
@@ -130,7 +130,7 @@ def add_to_dict(path, data, row):
 def get_oestat_choices():
     res = dict()
     with open(
-        "resources/OEFOS2012_DE_CTI.txt",
+        "/app/resources/OEFOS2012_DE_CTI.txt",
         newline="",
         encoding="latin1",
     ) as inp:
