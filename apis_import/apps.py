eb1f3aed0da8f6332a37123c2705e5bd66a648e2
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-08-11 09:50:39 +0200

    chore: ruff fixes

--- apis_import/apps.py
+++ apis_import/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class ApisImportConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apis_import'
-    verbose_name = 'APIS Import'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apis_import"
+    verbose_name = "APIS Import"
