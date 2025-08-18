dce44692c66526daf4ad1414b5fc813718c02b79
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-08-18 13:46:11 +0200

    fix: add funktion tab
    and smaller fixes in partial templates

---
+++ mine_frontend/templatetags/mine_extras.py
@@ -0,0 +1,18 @@
+from apis_core.apis_metainfo.models import Uri
+from django import template
+from django.utils.html import mark_safe
+
+register = template.Library()
+
+
+@register.filter()
+def mine_link(value):
+    if hasattr(value, "akademie_institution"):
+        if value.akademie_institution:
+            return mark_safe(f'<a href="/institution/{value.pk}">{value}</a>')
+    gnd = Uri.objects.filter(uri__contains="d-nb.info", object_id=value.pk)
+    if gnd.exists():
+        return mark_safe(
+            f'<a href="{gnd.first().uri}">{value}</a><i data-feather="external-link" style="width: 1.1em; height: 1.1em; padding-left: 0.2em; vertical-align: middle;"></i>'
+        )
+    return value
