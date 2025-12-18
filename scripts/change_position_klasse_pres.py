80801df987a1c29920c7159f58a6c49bdee5aa95
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-12-18 11:32:07 +0100

    chore: add missing scripts dir

---
+++ scripts/change_position_klasse_pres.py
@@ -0,0 +1,32 @@
+import logging
+
+import django
+
+django.setup()
+from apis_ontology.models import Institution, PositionAn
+
+# Create new positions "Präsident(in) Klasse" and "Vizepräsident(in) Klasse"
+
+logger = logging.getLogger(__name__)
+
+
+def run_migration():
+    insts = Institution.objects.filter(
+        label__in=[
+            "PHILOSOPHISCH-HISTORISCHE KLASSE",
+            "MATHEMATISCH-NATURWISSENSCHAFTLICHE KLASSE",
+        ]
+    ).values_list("id")
+    pos = PositionAn.objects.filter(
+        position__in=["Präsident(in)", "Vizepräsident(in)", "Sekretär(in)"],
+        obj_object_id__in=insts,
+    )
+    logger.info(f"found {pos.count()} positions, changing now")
+    for rel in pos:
+        logger.info(f"changing {pos}")
+        rel.position += " Klasse"
+        rel.save()
+
+
+if __name__ == "__main__":
+    run_migration()
