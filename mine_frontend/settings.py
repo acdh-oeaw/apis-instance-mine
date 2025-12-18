31d8c231a617bb53b8cfb378c69fc726cc465c6e
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2025-12-18 11:16:21 +0100

    fix: add facet for functions in presidium
    resolves #151

---
+++ mine_frontend/settings.py
@@ -0,0 +1,9 @@
+# Positionen im Präsidium
+POSITIONEN_PRES = [
+    "Präsident(in)",
+    "Vizepräsident(in)",
+    "Generalsekretär(in)",
+    "Sekretär(in)",
+    "Präsident(in) Klasse",
+    "Sekretär(in) Klasse",
+]
