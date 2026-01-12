d76e36f5c9f76cbbae235fbe5a349195da9a94da
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2026-01-12 19:20:46 +0100

    backup

--- mine_frontend/static/mine_frontend/js/date_slider.js
+++ mine_frontend/static/mine_frontend/js/date_slider.js
@@ -61,9 +61,41 @@ function toggleIncludeExclude(event) {
   console.log(templ, templ.content);
   const clone = document.importNode(templ.content, true);
   span_node.replaceChildren(clone);
-  const span_1 = sel.querySelector("span#" + kind + "-year");
-  console.log("span1", span_1);
-  span_1.textContent = val;
+  for (const elem of sel.querySelectorAll("span#" + kind + "-year")) {
+    elem.textConytent = val;
+  }
+}
+function fillDatesForm(e) {
+  console.log(e);
+  const form = e.target;
+  for (const elem of form.getElementsByClassName("mine-date-slider")) {
+    console.log(elem);
+    console.log(elem.dataset.formLower);
+    var form_lower_name = elem.dataset.formLower;
+    if (
+      elem
+        .querySelector("div.noUi-handle-lower > div.noUi-tooltip")
+        .classList.contains("exclusive")
+    ) {
+      console.log("hit");
+      form_lower_name += "_exclusive";
+    }
+    var form_upper_name = elem.dataset.formUpper;
+    if (
+      elem
+        .querySelector("div.noUi-handle-upper > div.noUi-tooltip")
+        .classList.contains("exclusive")
+    ) {
+      console.log("hit");
+      form_upper_name += "_exclusive";
+    }
+    const form_lower = form.querySelector(`input[name="${form_lower_name}"]`);
+    const form_upper = form.querySelector(`input[name="${form_upper_name}"]`);
+    const values = elem.noUiSlider.get();
+    console.log(values);
+    form_lower.value = parseInt(values[0]);
+    form_upper.value = parseInt(values[1]);
+  }
 }
 function startDateSliders(e) {
   console.log("script started");
@@ -72,6 +104,9 @@ function startDateSliders(e) {
     console.log(els.item(i));
     const item = els.item(i);
     initDateSlider(item.id, item.dataset.min, item.dataset.max);
+    const form = item.closest("form");
+    console.log(form);
+    form.addEventListener("submit", fillDatesForm);
   }
 }
 function updateYearAfterMove(
@@ -84,9 +119,12 @@ function updateYearAfterMove(
 ) {
   console.log(values, handle, tap, noUiSlider.target);
   if (handle === 0) {
-    noUiSlider.target.parentElement.querySelector(
+    const year = wNumb({ decimals: 0 }).to(parseFloat(values[0]));
+    for (const element of noUiSlider.target.parentElement.querySelectorAll(
       "span#lower-year",
-    ).textContent = wNumb({ decimals: 0 }).to(parseFloat(values[0]));
+    )) {
+      element.textContent = year;
+    }
   } else {
     noUiSlider.target.parentElement.querySelector(
       "span#upper-year",
