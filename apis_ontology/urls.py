934919a3e575a6e1f18284fe7e94ac3f74b3f85e
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2026-06-29 17:43:18 +0200

    feat: change url of frontend to base

--- apis_ontology/urls.py
+++ apis_ontology/urls.py
@@ -15,12 +15,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from apis_acdhch_default_settings.urls import urlpatterns
+from apis_acdhch_default_settings.views import Imprint
+from django.apps import apps
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
+from django.views.generic import TemplateView
+
+from apis_core.apis_entities.api_views import GetEntityGeneric
+
+urlpatterns = [
+    path("apis/", include("apis_core.urls", namespace="apis")),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("entity/<int:pk>/", GetEntityGeneric.as_view(), name="GetEntityGenericRoot"),
+    path("admin/", admin.site.urls),
+]
+urlpatterns += staticfiles_urlpatterns()
+
+if apps.is_installed("apis_bibsonomy"):
+    urlpatterns.append(
+        path("bibsonomy/", include("apis_bibsonomy.urls", namespace="bibsonomy"))
+    )
+
+urlpatterns.append(path("edit/", TemplateView.as_view(template_name="base.html")))
+urlpatterns.append(path("imprint", Imprint.as_view(), name="imprint"))
+urlpatterns += [
+    path("", include("apis_acdhch_django_auditlog.urls")),
+]
+
 
 urlpatterns += [path("", include("mine_frontend.urls"))]
 urlpatterns += [path("", include("apis_acdhch_django_invite.urls"))]
 urlpatterns += [path("", include("django_interval.urls"))]
-urlpatterns += [
-    path("", include("apis_acdhch_django_auditlog.urls")),
-]
